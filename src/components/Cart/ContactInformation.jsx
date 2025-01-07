cd346d230d6d782bd8bcd1ae32bdd082929cc2db
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:50:00 +0700

    принимает в себя FormInput, запришвает имя фамилия

--- src/components/Cart/ContactInformation.jsx
+++ src/components/Cart/ContactInformation.jsx
@@ -1,19 +1,20 @@
+import FormInput from "../FormInput";
+
 const ContactInformation = () => {
   return (
-    <div className="flex justify-between gap-5 mb-10">
-      <input
-        type="text"
-        name=""
-        id=""
-        placeholder="Ваше имя"
-        className="w-full p-3 border-2 rounded-2xl border-gray-300"
+    <div className="flex justify-between gap-5 mb-6">
+      <FormInput
+        type={"text"}
+        name={""}
+        id={""}
+        placeholder={"Ваше имя"}
       />
-      <input
-        type="text"
-        name=""
-        id=""
-        placeholder="Ваш телефон"
-        className="w-full p-3 border-2 rounded-2xl border-gray-300"
+      <FormInput
+        type={"number"}
+        name={"phoneNumber"}
+        id={""}
+        placeholder={"Ваш телефон"}
+        maxCountSymbol={11}
       />
     </div>
   );
