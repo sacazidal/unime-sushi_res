afc96a3685a4c8ee5f81ea9f4411f6abbcb38377
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:49:22 +0700

    компонент для удобства с адресом доставки

--- src/components/Cart/DeliveryAddress.jsx
+++ src/components/Cart/DeliveryAddress.jsx
@@ -1,9 +1,19 @@
+import LocationInformation from "./LocationInformation";
+import Maps from "./Maps";
 import ShippingMethod from "./ShippingMethod";
 
 const DeliveryAddress = () => {
   return (
-    <div className="w-2/5">
-      <ShippingMethod />
+    <div className="mb-6">
+      <div className="w-2/5">
+        <ShippingMethod />
+      </div>
+      <div className="w-full">
+        <LocationInformation />
+      </div>
+      <div className="w-full">
+        <Maps />
+      </div>
     </div>
   );
 };
