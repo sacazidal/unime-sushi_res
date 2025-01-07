ae30ad5b67bac948f0193167df63f3eba1fc518f
sacazidal <tw1nkle001@yandex.ru>
2025-01-07 14:43:27 +0700

    пофиксил баги скролла, закончил делать бургер меню

--- src/components/Header/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu.jsx
@@ -1,24 +1,63 @@
-import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdOutlineClose } from "react-icons/md";
+import BtnFooter from "../BtnFooter";
+import { useEffect } from "react";
 
-const BurgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+// eslint-disable-next-line react/prop-types
+const BurgerMenu = ({ isOpen, toggleIsOpenMenu }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
-  const toggleIsOpenMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
 
   return (
-    <div className="md:hidden flex flex-col">
-      <div className="flex flex-col">
-        <button className="" onClick={toggleIsOpenMenu}>
+    <div className="md:hidden flex flex-col relative">
+      <div className="flex flex-col relative">
+        <button className="z-50" onClick={toggleIsOpenMenu}>
           {isOpen ? (
-            <MdOutlineClose className="w-7 h-7" />
+            <MdOutlineClose className="w-7 h-7 fixed top-3 z-30" />
           ) : (
             <RxHamburgerMenu className="w-7 h-7" />
           )}
         </button>
+        {/* Оверлей */}
+        {isOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-20"
+            onClick={toggleIsOpenMenu}
+          ></div>
+        )}
+
+        {/* Меню */}
+        <div
+          className={`fixed top-0 left-0 h-full w-64 bg-white z-20 transform transition-transform duration-300 ease-in-out ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <div className="flex flex-col items-center gap-2 h-screen justify-center">
+            <BtnFooter id="роллы" title="Роллы" />
+            <BtnFooter
+              id="запеченные-роллы"
+              title="Запеченные роллы"
+            />
+            <BtnFooter
+              id="горячие-роллы"
+              title="Горячие роллы"
+            />
+            <BtnFooter id="сеты" title="Сеты" />
+            <BtnFooter id="закуски" title="Закуски" />
+            <BtnFooter id="вок" title="Вок" />
+            <BtnFooter id="пицца" title="Пицца" />
+            <BtnFooter id="напитки" title="Напитки" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,27 @@ import SocialLinks from "./SocialLinks";
 import Basket from "./Basket";
 import LocationNumber from "./LocationNumber";
 import BurgerMenu from "./BurgerMenu";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpenMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="my-3 md:my-7 px-4 xl:px-0">
-      <div className="max-w-screen-xl mx-auto flex items-center justify-between ">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between">
         <div className="flex gap-10 justify-between items-center">
           {/* Кнопка USER */}
           <BtnSignUp />
           {/* Локация и номер телефона */}
           <LocationNumber />
           {/* Бургер меню */}
-          <BurgerMenu />
+          <BurgerMenu
+            isOpen={isOpen}
+            toggleIsOpenMenu={toggleIsOpenMenu}
+          />
         </div>
         {/* Лого */}
         <Logo />
