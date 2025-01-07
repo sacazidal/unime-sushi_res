3e577400ee63bf9059a7da3eee5b2cfdb18f9f8f
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:39:50 +0700

    создал отдельны компонент для удобства

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,25 +1,11 @@
-import CartTitle from "../components/Cart/CartTitle";
-import ContactInformation from "../components/Cart/ContactInformation";
-import DeliveryAddress from "../components/Cart/DeliveryAddress";
+import Left from "../components/LeftPartCart/Left";
+import Right from "../components/RightPartCart/Right";
 
 const Cart = () => {
   return (
     <div className="max-w-screen-xl mx-auto px-4 xl:px-0 my-12 grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-14">
-      <div className="flex flex-col w-full px-10 md:px-0">
-        <div className="flex flex-col">
-          <div className="flex flex-col">
-            <CartTitle title={"Контактные данные"} />
-            <ContactInformation />
-          </div>
-          <div className="flex flex-col">
-            <CartTitle title={"Адрес доставки"} />
-            <DeliveryAddress />
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col w-full px-10 md:px-5 lg:px-10">
-        <CartTitle title={"Ваш заказ"} />
-      </div>
+      <Left />
+      <Right />
     </div>
   );
 };
