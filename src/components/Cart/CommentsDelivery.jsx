d81376989df72d65f206615b4cb2b24331646a6c
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:50:51 +0700

    Комментарий для заказа

---
+++ src/components/Cart/CommentsDelivery.jsx
@@ -0,0 +1,15 @@
+const CommentsDelivery = () => {
+  return (
+    <div className="w-full">
+      <input
+        type={"text"}
+        name={""}
+        id={"id"}
+        placeholder={"Введите текст"}
+        className="w-full min-h-8 border-none placeholder:border-none rounded-xl bg-gray-100 p-3 placeholder:font-montserrat placeholder:font-medium placeholder:text-xs font-montserrat font-medium text-xs"
+      />
+    </div>
+  );
+};
+
+export default CommentsDelivery;
