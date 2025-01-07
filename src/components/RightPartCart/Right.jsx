36786e18dd477093fec71d3980003a5906cc630a
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:42:25 +0700

    создал комп. правой части страницы корзины

---
+++ src/components/RightPartCart/Right.jsx
@@ -0,0 +1,36 @@
+import CartTitle from "../Cart/CartTitle";
+import FormInput from "../FormInput";
+import OrderItems from "./OrderItems";
+import PlaceAnOrder from "./PlaceAnOrder";
+
+const Right = () => {
+  return (
+    <div className="flex flex-col w-full gap-10">
+      <div className="flex flex-col w-full border-b rounded-b-[30px] pb-12 px-10 md:px-5 lg:px-10">
+        <CartTitle title={"Ваш заказ"} />
+        <div className="">
+          <OrderItems />
+        </div>
+        <div className="">
+          <PlaceAnOrder />
+        </div>
+      </div>
+      <div className="flex flex-col w-full border-b rounded-b-[30px] min-h-40 px-10 md:px-5 lg:px-10">
+        <CartTitle title={"Промокод"} />
+        <div className="flex gap-5">
+          <FormInput
+            type={"text"}
+            placeholder={"Промокод"}
+            name={""}
+            id={""}
+          />
+          <button className="font-montserrat font-semibold text-base text-white bg-orange-600 px-3 py-2 rounded-lg">
+            Применить
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Right;
