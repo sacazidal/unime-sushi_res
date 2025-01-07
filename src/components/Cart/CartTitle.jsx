7f949af9595747a73ce9c54e9bf6762119fa5ff3
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 03:51:23 +0700

    компонент для переиспользования титлов

--- src/components/Cart/CartTitle.jsx
+++ src/components/Cart/CartTitle.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const CartTitle = ({ title }) => {
   return (
-    <div className="font-montserrat font-semibold text-2xl mb-6">
+    <div className="font-montserrat font-semibold text-2xl mb-4">
       {title}
     </div>
   );
