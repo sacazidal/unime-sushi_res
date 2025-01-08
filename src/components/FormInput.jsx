f5c301a11bfd0544b75607c7a92dd71b667ae2ad
sacazidal <tw1nkle001@yandex.ru>
2025-01-08 18:18:56 +0700

    добавил value and onChange

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,24 +1,22 @@
 /* eslint-disable react/prop-types */
-const FormInput = ({ type, name, id, placeholder }) => {
-  const handleInputChange = (e) => {
-    if (name === "phoneNumber") {
-      const value = e.target.value;
-      e.target.value = value.slice(0, 11);
-    }
-    if (name === "name") {
-      const value = e.target.value;
-      e.target.value = value.slice(0, 11);
-    }
-  };
+const FormInput = ({
+  type,
+  name,
+  id,
+  placeholder,
+  value,
+  onChange,
+}) => {
   return (
     <input
       type={type}
       name={name}
       id={id}
       placeholder={placeholder}
+      value={value}
       className="w-full p-2 md:p-3 border-2 rounded-2xl border-gray-300 placeholder:font-montserrat placeholder:font-medium placeholder:text-xxs md:placeholder:text-xs font-montserrat font-medium text-xs flex placeholder:justify-center"
       maxLength={name === "phoneNumber" ? 11 : undefined}
-      onChange={handleInputChange}
+      onChange={onChange}
     />
   );
 };
